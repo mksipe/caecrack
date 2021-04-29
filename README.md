17232201688c27e2d3f94b337d4e7ee09cf3e9a9
Mason Sipe <mason@localhost.localdomain>
2021-04-29 15:55:11 -0500

    github stuff

---
+++ README.md
@@ -0,0 +1,20 @@
+# caecrack
+
+caecrack is a small project I developed to get more familiar with rust. It is an automatic ROT solver. It uses a pre-defined wordlist to start cracking against a specific string or word.
+
+This program is intended to tell you the number of rotations rather than tell you the plaintext string. To do this, it takes the first word of a sentence and solves that before everything else.
+
+---
+
+## Installation
+
+Prerequisites:
+
+|system|cargo|
+|-|-|
+|cargo|clap|
+|wordlist|stdlib|
+
+---
+
+
---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+if [ $(/usr/bin/id -u) -ne 0 ] ; then
+    printf "You must be root.\n"
+    exit
+elif [ $(/usr/bin/id -u) -eq 0 ]; then
+    if [ $(which cargo > /dev/null ; echo $?) -ne 0 ]; then
+        printf "Cargo is not installed.\n"
+    elif [ $(which cargo > /dev/nulll; echo $?) -eq 0 ]; then
+        printf "Making installation.\n"
+        printf "Compiling binaries...\n" 
+        cargo build
+        cp target/debug/caecrack /usr/bin/ > /dev/null
+       	if [ $(which hts4 > /dev/null; echo $?) -eq 0 ]; then
+		printf "`caecrack -V`, has been installed.\n"
+	elif [ $(which hts4 > /dev/null; echo $?) -ne 0 ]; then
+		printf "caecrack could not be installed.\n"
+	fi
+    fi
+fi
